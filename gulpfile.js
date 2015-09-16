afb3e7d51705f493b4611886eac4d405fe75f160
Torleif Halseth <halseth.torleif@gmail.com>
2015-09-16 13:56:10 +0200

    Using hbs for views

--- gulpfile.js
+++ gulpfile.js
@@ -65,6 +65,6 @@ gulp.task('kss', function () {
 });
 
 gulp.task('default', ['fonts', 'sass', 'sass-styleguide', 'kss', 'browser-sync'], function() {
-  gulp.watch('./sass/**/*.scss', ['sass', 'sass-styleguide', 'kss']);
+  gulp.watch(['./sass/**/*.scss', './sass/**/*.hbs', './template/index.html'], ['sass', 'sass-styleguide', 'kss']);
   gulp.watch('./styleguide/index.html').on('change', browserSync.reload);
 });
