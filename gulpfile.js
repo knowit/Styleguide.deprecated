2273a75b6f8627c06ff37b100a2dfa741f748fd3
Torleif Halseth <halseth.torleif@gmail.com>
2016-01-29 14:54:11 +0100

    Use same css file for both styleguide and "solution"

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,8 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var exec = require('child_process').exec;
 var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
 var scsslint = require('gulp-scss-lint');
-
-var styleguide = {
-  sassFilePath: './sass/styleguide.scss',
-  cssFilePath: './styleguide/css/styleguide.css',
-  cssDirectory: './styleguide/css',
-  cssFileName: 'styleguide.css'
-};
-var style = {
-  sassFilePath: './sass/screen.scss',
-  cssFilePath: './styleguide/css/screen.css',
-  cssDirectory: './css',
-  cssFileName: 'screen.css'
-};
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('fonts', function() {
   gulp.src('./node_modules/font-awesome/fonts/*.*')
@@ -26,23 +13,24 @@ gulp.task('fonts', function() {
 });
 
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: './styleguide'
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: './styleguide'
+    }
+  });
 });
 
-gulp.task('sass', function () {
-  gulp.src(style.sassFilePath)
+gulp.task('sass', function() {
+  gulp.src('./sass/screen.scss')
     .pipe(sass({
       includePaths: require('node-neat').includePaths
     }).on('error', sass.logError))
     .pipe(sourcemaps.init())
     .pipe(autoprefixer())
-    .pipe(concat(style.cssFileName))
+    .pipe(minifyCss())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(style.cssDirectory))
+    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./styleguide/css'))
     .pipe(browserSync.stream());
 });
 
@@ -54,30 +42,17 @@ gulp.task('sass-lint', function() {
     }));
 });
 
-gulp.task('sass-styleguide', function () {
-  gulp.src(styleguide.sassFilePath)
-    .pipe(sass({
-      includePaths: require('node-neat').includePaths
-    }).on('error', sass.logError))
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer())
-    .pipe(concat(styleguide.cssFileName))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(styleguide.cssDirectory))
-    .pipe(browserSync.stream());
-});
-
-gulp.task('kss', function () {
-  exec('npm run kss', function (err) {
-    if(err !== null) {
+gulp.task('kss', function() {
+  exec('npm run kss', function(err) {
+    if (err !== null) {
       console.log(err);
     }
   });
 });
 
-gulp.task('default', ['fonts', 'sass-lint', 'sass', 'sass-styleguide', 'kss', 'browser-sync'], function() {
+gulp.task('default', ['fonts', 'sass-lint', 'sass', 'kss', 'browser-sync'], function() {
   gulp.watch(['./sass/**/*.hbs', './template/index.html'], ['kss'])
-  gulp.watch(['./sass/**/*.scss'], ['sass-lint','sass', 'sass-styleguide']);
+  gulp.watch(['./sass/**/*.scss'], ['sass-lint', 'sass']);
   gulp.watch(['./readme.md'], ['kss']);
   gulp.watch('./styleguide/index.html').on('change', browserSync.reload);
 });
